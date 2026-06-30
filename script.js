43d0d1fbfef74443262c5a59d7e1e307bf62aeb5
sumanthreddy2279-a11y <sumanthreddy2279@gmail.com>
2026-06-30 16:09:19 +0530

    Refactor script.js for improved readability

--- script.js
+++ script.js
@@ -187,10 +187,10 @@ hero.innerHTML="🌙 Good Evening, Welcome!";
 // ===============================
 
 const galleryImages = [
-    "project1.jpg.png",
-    "project2.jpg.png",
-    "project3.jpg.png",
-    "project4.jpg.png",
+    "project1.png.jpeg",
+    "project2.png.jpeg",
+    "project3.png.jpeg",
+    "project4.png.jpeg",
 ];
 
 let currentImage = 0;
